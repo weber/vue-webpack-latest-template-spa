72ec667013bc9a223682cc1cac35aa81a5c69a2f
webs <rainbluz@gmail.com>
2018-05-06 19:02:45 +0300

    add event bus

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import router from './router'
 import App from './App'
 // import { store } from './store'
 // import Ramda from './vendor/ramda-proxy'
-// import EventBus from './utils/eventBus'
+import EventBus from './utils/eventBus'
 
 // import './styles/app.styl'
 import '@/styles/app.scss'
 
-// Vue.use(EventBus)
+Vue.use(EventBus)
 // Vue.use(Ramda)
 
 /* eslint-disable-next-line no-new */
