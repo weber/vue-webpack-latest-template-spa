586f9519d32860171e12776e0dd6eeb5060ec313
webs <rainbluz@gmail.com>
2018-05-06 19:09:19 +0300

    add vuex

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App'
-// import { store } from './store'
+import { store } from './state'
 // import Ramda from './vendors-proxy/ramda-proxy'
 import EventBus from './utils/eventBus'
 
-// import './styles/app.styl'
 import '@/styles/app.scss'
 
 Vue.use(EventBus)
@@ -15,6 +14,6 @@ Vue.use(EventBus)
 new Vue({
   el: '#app',
   router,
-  // store,
+  store,
   render: h => h(App)
 })
